7cbbf63460ee8ac0690d98f0bb8d0f8383c1b1a7
RickeyWard <rickeyward@diamonddrake.com>
2020-09-26 13:02:08 -0400

    Updates

---
+++ precache-manifest.28909c19a04366ccec5a9c08c4f03319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd8116d8c549441e0a20b55cb8f5cf0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b45e7f434a11e4fedc5b",
+    "url": "./static/css/main.95022062.chunk.css"
+  },
+  {
+    "revision": "437975834c79a1a37ce5",
+    "url": "./static/js/2.40e58367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.40e58367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45e7f434a11e4fedc5b",
+    "url": "./static/js/main.4bf543d3.chunk.js"
+  },
+  {
+    "revision": "710f6b41f763b7782afb",
+    "url": "./static/js/runtime-main.72af0725.js"
+  }
+]);
