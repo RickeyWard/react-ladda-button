8554b85266bb4cea8e28049a815eda958ddffe3c
RickeyWard <rickeyward@diamonddrake.com>
2020-09-28 12:46:42 -0400

    Started adding testing
    issue: generates a types.d.ts file for tests but shoudln't

---
+++ src/__tests__/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import LaddaButton, { EXPAND_LEFT } from '..'
+
+describe('<LaddaButton />', () => {
+  test("label text rendered from children prop", ()=>{
+    const testtext = 'test button'
+    const { getByText } = render(
+      <LaddaButton data-style={EXPAND_LEFT}>{testtext}</LaddaButton>
+    )
+    expect(getByText(testtext)).toHaveTextContent(testtext)
+  })
+})
