32bb4e78e94e7e440062a4d0f5cfecc46761385e
RickeyWard <rickeyward@diamonddrake.com>
2020-09-28 16:57:26 -0400

    working on tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import LaddaButton, { EXPAND_LEFT } from '..'
 
@@ -10,5 +10,6 @@ describe('<LaddaButton />', () => {
       <LaddaButton data-style={EXPAND_LEFT}>{testtext}</LaddaButton>
     )
     expect(getByText(testtext)).toHaveTextContent(testtext)
+    cleanup()
   })
 })
